9f722e2197f4379ce9d65697b4456872838816c4
mtext <mtext@5e5cd243-3975-4a28-937b-384570c9047d>
2022-01-18 07:33:25 +0000

    translated german phrases and comments

--- Framework_Training/Scripts/Grafics.js
+++ Framework_Training/Scripts/Grafics.js
@@ -6,18 +6,18 @@ function snapBarGraph(){
 load('Scripts/env.rhino.1.2.js', true);
 load('Scripts/snap.svg-min.js', true);
 
-// Grafikobjekt in angegebener Größe initialisieren
+// Initialize graphic object in defined size
 
-// Variablen anlegen
-	// Höhe der Grafik
+// Initialize variables
+	// Graphic height
 	var GraphicHeight = 320;
-	// Position der Ausgabe auf der Vertikalen Achse
+	// Output position on the vertical axis
 	var StartPosY = 305;
 	var SpaceY = 50;
 
-	var Multiplier = 20; // Umrechnungsfaktor für den Verbrauchswert
+	var Multiplier = 20; // Conversion factor for the consumption value
 			
-	// Festlegen von Stilen für Text, Linien, aktueller Verbrauch und Vorjahresverbrauch
+	// Set styles for text, lines, current consumption and previous year consumption
 	var textAttr = {"font-family": "SANS","font-size": "8pt",};
 	var lineAttr = {stroke: "#999", strokeWidth: 0.5, strokeLinecap:"round"};
 	var AktAttr = {"fill": '#0071BB'};
@@ -28,19 +28,19 @@ load('Scripts/snap.svg-min.js', true);
 	var averagetAttr = {"fill": '#FF7878'};
 	var badAttr = {"fill": '#780000'};	
 
-	// Basisdaten für den Vergleich
+	// Basic data for comparison
 	var ONE = [500, 1100, 1900, 2500];
 	var TWO = [1000, 2100, 3000, 4100];
 	var THREE = [1400, 2500, 3800, 5000];
 	var FOUR = [1800, 3000, 4300, 6000];	
 
 
-// Anlegen der Grafik, die Höhe 
+// Create the graphic, height
 	var snap = Snap(650,GraphicHeight);
 
-	var countX = 7; // Anzahl der Linien zum ausgeben	
-	var LabelText = 6000; // Höchster Wert der Y-Achse
-	var AktPosY = 5; // Space zum oberen Grafikrand
+	var countX = 7; // amount of lines to print
+	var LabelText = 6000; // Highest value on the Y-axis
+	var AktPosY = 5; // Space/distance to the upper graphic border
 	
 	for (var i=0; i < countX; i++) {
 		snap.line( 30, AktPosY, 500, AktPosY ).attr(lineAttr);
@@ -49,37 +49,35 @@ load('Scripts/snap.svg-min.js', true);
 		LabelText = (LabelText - 1000);
 	}
 
-	snap.text(30, GraphicHeight, "Aktuell").attr(textAttr);
-	snap.text(75, GraphicHeight, "Vorjahr").attr(textAttr);
+	snap.text(30, GraphicHeight, "Current year").attr(textAttr);
+	snap.text(75, GraphicHeight, "Previous year").attr(textAttr);
 	snap.text(140, GraphicHeight, "1 Person").attr(textAttr);
-	snap.text(240, GraphicHeight, "2 Personen").attr(textAttr);
-	snap.text(340, GraphicHeight, "3 Personen").attr(textAttr);
-	snap.text(440, GraphicHeight, "4 Personen").attr(textAttr);
+	snap.text(240, GraphicHeight, "2 Persons").attr(textAttr);
+	snap.text(340, GraphicHeight, "3 Persons").attr(textAttr);
+	snap.text(440, GraphicHeight, "4 Persons").attr(textAttr);
 	
-	/** Ausgabe der Balken für den Aktuellen und Vorjahresverbrauch  **/
-
-	
-	// Ein Personen Haushalt
+	/** Output of bars for current and previous year consumption  **/
+	// 1-person household
 	snap.rect(120, StartPosY-(ONE[0]/Multiplier), 20, ONE[0]/Multiplier).attr(goodAttr);
 	snap.rect(140, StartPosY-(ONE[1]/Multiplier), 20, ONE[1]/Multiplier).attr(okAttr);
 	snap.rect(160, StartPosY-(ONE[2]/Multiplier), 20, ONE[2]/Multiplier).attr(averagetAttr);
 	snap.rect(180, StartPosY-(ONE[3]/Multiplier), 20, ONE[3]/Multiplier).attr(badAttr);
-	// Zwei Personen Haushalt
+	// 2-person household
 	snap.rect(220, StartPosY-(TWO[0]/Multiplier), 20, TWO[0]/Multiplier).attr(goodAttr);
 	snap.rect(240, StartPosY-(TWO[1]/Multiplier), 20, TWO[1]/Multiplier).attr(okAttr);
 	snap.rect(260, StartPosY-(TWO[2]/Multiplier), 20, TWO[2]/Multiplier).attr(averagetAttr);
 	snap.rect(280, StartPosY-(TWO[3]/Multiplier), 20, TWO[3]/Multiplier).attr(badAttr);
-	// Drei Personen Haushalt
+	// 3-person household
 	snap.rect(320, StartPosY-(THREE[0]/Multiplier), 20, THREE[0]/Multiplier).attr(goodAttr);
 	snap.rect(340, StartPosY-(THREE[1]/Multiplier), 20, THREE[1]/Multiplier).attr(okAttr);
 	snap.rect(360, StartPosY-(THREE[2]/Multiplier), 20, THREE[2]/Multiplier).attr(averagetAttr);
 	snap.rect(380, StartPosY-(THREE[3]/Multiplier), 20, THREE[3]/Multiplier).attr(badAttr);
-	// Vier Personen Haushalt
+	// 4-person household
 	snap.rect(420, StartPosY-(FOUR[0]/Multiplier), 20, FOUR[0]/Multiplier).attr(goodAttr);
 	snap.rect(440, StartPosY-(FOUR[1]/Multiplier), 20, FOUR[1]/Multiplier).attr(okAttr);
 	snap.rect(460, StartPosY-(FOUR[2]/Multiplier), 20, FOUR[2]/Multiplier).attr(averagetAttr);
 	snap.rect(480, StartPosY-(FOUR[3]/Multiplier), 20, FOUR[3]/Multiplier).attr(badAttr);
 
-	// Grafik bereitstellen
+	// Allocate graphic
 	return 'data:image/svg+xml;base64,' + window.btoa(snap.toString());
 }
